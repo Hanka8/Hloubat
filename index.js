ed3135f88dd0f09aa1d4cc244bf48a3385d9c114
Hanka8 <hanka.maruskevicova@gmail.com>
2024-08-31 17:36:26 +0200

    first steps...

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 const hideOnMobileWidth = 768;
 
-const elementsHiddenOnMobile = document.querySelectorAll('[data-custom="hideOnMobile"]');
+const elementsHiddenOnMobile = document.querySelectorAll(
+  '[data-custom="hideOnMobile"]'
+);
 const hamburgerBtn = document.querySelector('[data-custom="hamburgerBtn"]');
-const hamburgerLines = hamburgerBtn.querySelectorAll('span');
+const hamburgerLines = hamburgerBtn.querySelectorAll("span");
 const navMenu = document.querySelector('[data-custom="navMenu"]');
 
 function hideOnMobile(elements) {
-    if (window.innerWidth < hideOnMobileWidth) {
-        elements.forEach(function(element) {
-            element.style.display = 'none';
-        });
-    } else {
-        elements.forEach(function(element) {
-            element.style.display = 'block';
-        });
-    }
+  if (window.innerWidth < hideOnMobileWidth) {
+    elements.forEach(function (element) {
+      element.style.display = "none";
+    });
+  } else {
+    elements.forEach(function (element) {
+      element.style.display = "block";
+    });
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    hideOnMobile(elementsHiddenOnMobile);
+document.addEventListener("DOMContentLoaded", () => {
+  hideOnMobile(elementsHiddenOnMobile);
 });
-window.addEventListener('resize', () => {
-    hideOnMobile(elementsHiddenOnMobile);
-});
-
-hamburgerBtn.addEventListener('click', () => {
-    hamburgerBtn.classList.toggle('close');
-    hamburgerLines.forEach(function(line) {
-        line.classList.toggle('close');
-    });
-    navMenu.classList.toggle('open');
+window.addEventListener("resize", () => {
+  hideOnMobile(elementsHiddenOnMobile);
 });
 
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.toggle("close");
+  hamburgerLines.forEach(function (line) {
+    line.classList.toggle("close");
+  });
+  navMenu.classList.toggle("open");
+});
 
-// FOR DEBUGGING ONLY   
+// TODO: swipe and touch events for mobile
+// TODO: clicking outside the menu closes it
+
+
+// REMOVE THE FOLLOWING CODE AFTER TESTING
 
 // console.log window screen aspect ratio
 
-// window.addEventListener('resize', () => {
-//     console.log("aspect ratio " + Math.round(window.innerWidth / window.innerHeight * 100) / 100);
+// window.addEventListener("resize", () => {
+//   console.log(
+//     "aspect ratio " +
+//       Math.round((window.innerWidth / window.innerHeight) * 100) / 100
+//   );
 // });
-
