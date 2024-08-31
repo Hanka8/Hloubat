1576c9f440ff02152794c5d7d53009a4623ed444
Hanka8 <hanka.maruskevicova@gmail.com>
2024-08-31 14:41:27 +0200

    save 1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const hideOnMobileWidth = 768;
 
 const elementsHiddenOnMobile = document.querySelectorAll('[data-custom="hideOnMobile"]');
+const hamburgerBtn = document.querySelector('[data-custom="hamburgerBtn"]');
+const hamburgerLines = hamburgerBtn.querySelectorAll('span');
+const navMenu = document.querySelector('[data-custom="navMenu"]');
 
 function hideOnMobile(elements) {
     if (window.innerWidth < hideOnMobileWidth) {
@@ -20,3 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('resize', () => {
     hideOnMobile(elementsHiddenOnMobile);
 });
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('close');
+    hamburgerLines.forEach(function(line) {
+        line.classList.toggle('close');
+    });
+    navMenu.classList.toggle('open');
+});
+
+
+// FOR DEBUGGING ONLY   
+
+// console.log window screen aspect ratio
+
+// window.addEventListener('resize', () => {
+//     console.log("aspect ratio " + Math.round(window.innerWidth / window.innerHeight * 100) / 100);
+// });
+
